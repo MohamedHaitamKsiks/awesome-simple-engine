8276a411d0cc121fb4bd79147e5717ff5dd33845
Mohamed Haitam Ksiks <ksiks_wa3r@pop-os.localdomain>
2023-02-28 17:52:49 +0100

    init the project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'myapplication' library on application startup.
+    static {
+        System.loadLibrary("myapplication");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'myapplication' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
