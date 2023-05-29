278517342e2ff750e3a1ad68eef2204275b60654
ksiks_wa3r <simpleteam2001@gmail.com>
2023-05-29 23:16:11 +0100

    start build.py

--- build.py
+++ build.py
@@ -1,4 +1,28 @@
 #build a porject to the platform given by the user
 import sys
+import os
+import shutil
 
-print(sys.argv);
+#expect 3 arguments
+assert(len(sys.argv) == 3)
+
+#get project path
+projectPath = sys.argv[1]
+
+#get platform
+platform = sys.argv[2]
+
+#check platform validity
+assert(platform in ("windows", "linux", "android"))
+
+#remove .tmp if exists
+if ".tmp" in os.listdir(os.getcwd()):
+    shutil.rmtree(".tmp")
+
+#craete and copy platfrom to .tmp
+shutil.copytree(f"platforms/{ platform }", ".tmp")
+
+#copy project + engine + dependencies to .tmp
+if platform == "android":
+    shutil.copytree("engine", ".tmp/app/src/main/cpp/engine")
+    
