35d195aeea9b62a9414d997923ed15bed6d6afc2
Mohamed Haitam Ksiks <ksiks_wa3r@pop-os.localdomain>
2023-09-13 13:48:42 +0100

    add sprite ecs

--- project-template/src/registry.cpp
+++ project-template/src/registry.cpp
@@ -2,5 +2,7 @@
 
 void ECSRegistry()
 {
-    
+	ComponentManager::RegisterComponent<SpriteRenderer2D>(UniqueString("SpriteRenderer2D"));
+	ComponentManager::RegisterComponent<Transform2D>(UniqueString("Transform2D"));
+	SystemManager::RegisterSystem<SpriteRenderingSystem2D>();
 }
--- project-template/src/registry.h
+++ project-template/src/registry.h
@@ -1,10 +1,12 @@
-// DO NOT DELETE
-
 #ifndef ASENGINE_ECS_REGISTRY_H
 #define ASENGINE_ECS_REGISTRY_H
 
 #include "ASEngine.h"
 using namespace ASEngine;
+
+#include "components/SpriteRenderer2D/SpriteRenderer2D.h"
+#include "components/Transform2D/Transform2D.h"
+#include "systems/SpriteRenderingSystem2D/SpriteRenderingSystem2D.h"
 
 // register ecs
 void ECSRegistry();
