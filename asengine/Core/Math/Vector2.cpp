e2716ccfc896bb38aaeb7934148abe332ed86ee7
Mohamed Haitam Ksiks <ksiks_wa3r@pop-os.localdomain>
2024-05-05 14:38:11 +0200

    Add Vector Serialization

--- asengine/Core/Math/Vector2.cpp
+++ asengine/Core/Math/Vector2.cpp
@@ -1,4 +1,6 @@
 #include "Vector2.h"
+#include "Core/Serialization/Json.h"
+#include "Core/Serialization/Serializer.h"
 
 namespace ASEngine
 {
@@ -14,6 +16,24 @@ namespace ASEngine
             x * cosAngle + y * sinAngle,
             -x * sinAngle + y * cosAngle
         };
+    }
+
+    // serialization of vector2
+    template <>
+    Json Serializer<Vector2>::Serialize(const Vector2 &value)
+    {
+        Json vector2Obj{{}};
+        vector2Obj["x"] = value.x;
+        vector2Obj["y"] = value.y;
+
+        return vector2Obj;
+    }
+    template <>
+    void Serializer<Vector2>::Deserialize(const Json &object, Vector2 &dest)
+    {
+        ASENGINE_ASSERT(object.is_object(), "Can't deserialize Vecto2 if it's not an object");
+        Serializer<float>::Deserialize(object.at("x"), dest.x);
+        Serializer<float>::Deserialize(object.at("y"), dest.y);
     }
 
 } // namespace ASEngine
--- asengine/Core/Math/Vector3.cpp
+++ asengine/Core/Math/Vector3.cpp
@@ -3,4 +3,23 @@
 namespace ASEngine
 {
 
+    // serialization of Vector3
+    template <>
+    Json Serializer<Vector3>::Serialize(const Vector3 &value)
+    {
+        Json vector3Obj{{}};
+        vector3Obj["x"] = value.x;
+        vector3Obj["y"] = value.y;
+        vector3Obj["z"] = value.z;
+
+        return vector3Obj;
+    }
+    template <>
+    void Serializer<Vector3>::Deserialize(const Json &object, Vector3 &dest)
+    {
+        ASENGINE_ASSERT(object.is_object(), "Can't deserialize Vector3 if it's not an object");
+        Serializer<float>::Deserialize(object.at("x"), dest.x);
+        Serializer<float>::Deserialize(object.at("y"), dest.y);
+        Serializer<float>::Deserialize(object.at("z"), dest.z);
+    }
 } // namespace ASEngine
