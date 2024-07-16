f7619585100b20efa119edf21a4579d4def26a12
Mohamed Haitam Ksiks <ksiks_wa3r@pop-os.localdomain>
2024-07-16 21:42:53 +0200

    add ASENGINE_API to InputEvent for windows

--- asengine/Core/InputSystem/InputEvent.cpp
+++ asengine/Core/InputSystem/InputEvent.cpp
@@ -3,20 +3,19 @@
 #include "Core/Error/Assertion.h"
 
 // generate getters and setters for event fields
-#define GENERATE_GETTERS_SETTERS(INPUT_TYPE) \
-    template <> \
-    void InputEvent::Set<INPUT_TYPE>(const INPUT_TYPE &event) \
-    { \
-        m_##INPUT_TYPE = event; \
-        m_Type = INPUT_TYPE::TYPE(); \
-    } \
-    template <> \
-    const INPUT_TYPE& InputEvent::Get<INPUT_TYPE>() const \
-    { \
+#define GENERATE_GETTERS_SETTERS(INPUT_TYPE)                                      \
+    template <>                                                                   \
+    ASENGINE_API void InputEvent::Set<INPUT_TYPE>(const INPUT_TYPE &event)        \
+    {                                                                             \
+        m_##INPUT_TYPE = event;                                                   \
+        m_Type = INPUT_TYPE::TYPE();                                              \
+    }                                                                             \
+    template <>                                                                   \
+    ASENGINE_API const INPUT_TYPE &InputEvent::Get<INPUT_TYPE>() const             \
+    {                                                                             \
         ASENGINE_ASSERT(INPUT_TYPE::TYPE() == m_Type, "Invalid InputEvent Type"); \
-        return m_##INPUT_TYPE; \
+        return m_##INPUT_TYPE;                                                    \
     }
-
 
 namespace ASEngine
 {
